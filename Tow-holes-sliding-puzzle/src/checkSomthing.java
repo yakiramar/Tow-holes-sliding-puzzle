183903238759e9ebd132b2ad0eb2cdc60d9ba10b
YakirA <YakirA@HMS1AMARYAKIR.hms.co.il>
2022-01-17 11:53:34 +0200

    first

---
+++ Tow-holes-sliding-puzzle/src/checkSomthing.java
@@ -0,0 +1,16 @@
+
+public class checkSomthing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[][]= new int[3][4];
+		for(int i =0; i< a.length; i++) {
+			for(int j = 0; j<a[i].length; j++) {
+				a[i][j]= 1;
+			}
+		}
+
+	}
+
+}
