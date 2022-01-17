641926664d884fbafc38a102acfa8aa4180c7224
YakirA <YakirA@HMS1AMARYAKIR.hms.co.il>
2022-01-17 21:18:54 +0200

    zcsz

--- Tow-holes-sliding-puzzle/src/Ex1.java
+++ Tow-holes-sliding-puzzle/src/Ex1.java
@@ -35,7 +35,7 @@ public class Ex1 {
 		
 		System.out.println("\n");
 	    movingBlock= p.towBlockLeft();
-	    System.out.println("jgfhgf");
+	    System.out.println("test");
 	    System.out.println("moving tow block sucsess2 ? "+movingBlock);
 	    p.printBoard(p.board);
 	    
